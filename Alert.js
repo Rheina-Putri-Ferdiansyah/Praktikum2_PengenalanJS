4d548ff91c4d5e66960f6541eeee9bad66d68b20
rheina putri ferdiansyah <63830493+Rheina-Putri-Ferdiansyah@users.noreply.github.com>
2020-09-08 12:20:57 +0700

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';  
+class Alert extends Component {  
+  render(){  
+    return (  
+      <div className="alert alert-success">  
+      <center>
+        <h4>React JS itu...</h4>  
+        seru dan menyenangkan 
+        </center> 
+      </div>  
+    );  
+  }  
+}  
+export default Alert;
